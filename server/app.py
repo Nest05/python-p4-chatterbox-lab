6b7be758d739ffdabbd5d8efde62b9c3381d55f0
Nestor Masinde <nestormasinde@gmail.com>
2024-04-11 22:54:17 +0300

    chatterbox lab covered

--- server/app.py
+++ server/app.py
@@ -14,13 +14,71 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = [message.to_dict() for message in Message.query.all()]
+        return make_response( messages, 200 )
+    
+    elif request.method == 'POST':
+        new_message = Message(
+            body=request.form.get("body"),
+            username=request.form.get("username"),
+        )
 
-@app.route('/messages/<int:id>')
+        db.session.add(new_message)
+        db.session.commit()
+
+        response = make_response(new_message.to_dict(), 201)
+        return response
+
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'] )
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        message_serialized = message.to_dict()
+
+        response = make_response ( 
+            message_serialized, 
+            200  
+        )
+
+        return response
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(message, attr, request.form.get(attr))
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_serialized = message.to_dict()
+
+        response = make_response ( 
+            message_serialized, 
+            200  
+        )
+
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Message deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
